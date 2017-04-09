60e9d4d8056b0b337b50b72ec718942c18aaf109
gregunz <mail@gregunz.io>
2017-04-09 19:09:39 +0200

    commit before leaving to lausanne

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -16,53 +16,7 @@ export class LoginPage {
     public navCtrl: NavController,
     public storage: Storage,
   ) {
-    Facebook.browserInit(this.FB_APP_ID, "v2.8");
   }
 
-  doFbLogin(){
-    let nav = this.navCtrl;
-    let storage = this.storage;
 
-    //the permissions your facebook app needs from the user
-    let permissions = ["public_profile", "user_friends"];
-
-    Facebook.login(permissions)
-      .then(function(response){
-        //let userId = response.authResponse.userID;
-        let params = [];
-
-        //Getting name property
-        Facebook.api("/me?fields=id,name,friends{id,name}", params)
-          .then((fbUser) => {
-
-            let user: User = {
-              id : fbUser.id,
-              name : fbUser.name,
-              pictureURL : "https://graph.facebook.com/" + fbUser.id + "/picture?type=large",
-              friends: fbUser.friends.data.map((friend) => {
-                return {
-                  id: friend.id,
-                  name: friend.name,
-                };
-              })
-            };
-
-            storage.set("user", user)
-              /*
-              .then(() => {
-                nav.push(UserPage);
-              })
-              */
-              .catch((error) => {
-                console.log("An error occured during the storage of the logged user:" + error);
-              });
-          })
-          .catch((error) => {
-            console.log("An error occured during the facebook api call:" + error);
-          });
-      })
-      .catch((error) => {
-        console.log("An error occured during the facebook login:" + error);
-      });
-  }
 }
